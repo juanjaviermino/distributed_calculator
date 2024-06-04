e5b60f0bed9ee76cf8b708594bcad28fcb904774
juanjaviermino <juan.mino.arboleda@udla.edu.ec>
2024-06-03 19:21:35 -0500

    Remove Origin from header

--- src/api.js
+++ src/api.js
@@ -2,11 +2,10 @@ import axios from 'axios';
 
 // Create an Axios instance
 const api = axios.create({
-    baseURL: 'https://pyi1egz3ki.execute-api.us-east-1.amazonaws.com/default', // Set your base URL here
+    baseURL: 'https://pyi1egz3ki.execute-api.us-east-1.amazonaws.com/default', 
     timeout: 10000, // Request timeout
     headers: {
-        'Content-Type': 'application/json',
-        'Origin': 'https://distributed-calculator.onrender.com'
+        'Content-Type': 'application/json'
     }
 });
 
