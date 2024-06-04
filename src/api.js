1c081f636e22c5d79ff5c998265014040c4b49a4
juanjaviermino <juan.mino.arboleda@udla.edu.ec>
2024-06-03 19:13:51 -0500

    Add origin to request

--- src/api.js
+++ src/api.js
@@ -6,6 +6,7 @@ const api = axios.create({
     timeout: 10000, // Request timeout
     headers: {
         'Content-Type': 'application/json',
+        'Origin': 'https://distributed-calculator.onrender.com'
     }
 });
 
