dc4465d99d8891089eb30bebab36802a77020ab0
juanjaviermino <juan.mino.arboleda@udla.edu.ec>
2024-06-03 19:10:23 -0500

    Chao

--- src/api.js
+++ src/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // Create an Axios instance
 const api = axios.create({
-    baseURL: 'https://pyi1egz3ki.execute-api.us-east-1.amazonaws.com/default', 
-    timeout: 100000,
+    baseURL: 'https://pyi1egz3ki.execute-api.us-east-1.amazonaws.com/default', // Set your base URL here
+    timeout: 10000, // Request timeout
     headers: {
         'Content-Type': 'application/json',
     }
