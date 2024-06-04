e96dedd0e799374b29c88abbc61a35ae999c3ac1
juanjaviermino <juan.mino.arboleda@udla.edu.ec>
2024-06-03 19:04:36 -0500

    hola

--- src/api.js
+++ src/api.js
@@ -3,19 +3,10 @@ import axios from 'axios';
 // Create an Axios instance
 const api = axios.create({
     baseURL: 'https://pyi1egz3ki.execute-api.us-east-1.amazonaws.com/default', 
-    timeout: 10000, // Request timeout
+    timeout: 100000,
     headers: {
         'Content-Type': 'application/json',
-        // Add more headers as needed here, for example:
-        'X-Requested-With': 'XMLHttpRequest', // This header can help with CORS
     }
-});
-
-// Interceptor to handle preflight
-api.interceptors.request.use(request => {
-    // Ensure credentials are sent with every request if needed
-    request.withCredentials = true;
-    return request;
 });
 
 export default api;
